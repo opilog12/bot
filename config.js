1bfa501fe1c60d0e5b37b5e3f9e3115a142c232a
Opilog <128095264+opilog12@users.noreply.github.com>
2025-02-01 11:55:32 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "mongodb+srv://opiloggames:MZ2P2HXB6v6HBW1v@musicbot.dcvr8.mongodb.net/?retryWrites=true&w=majority",
+  mongodbUri : "",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
