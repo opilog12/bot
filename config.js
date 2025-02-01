a2910e99dcea79f8e8b18c51710db6bd65a82467
Opilog <128095264+opilog12@users.noreply.github.com>
2025-02-01 11:42:28 +0100

    Update config.js

--- config.js
+++ config.js
@@ -15,17 +15,17 @@ module.exports = {
   errorLog: "", 
   nodes: [
      {
-  "identifier": "INZEWORLD.COM (DE)",
-  "password": "saher.inzeworld.com",
-  "host": "lava.inzeworld.com",
-  "port": 3128,
-  "secure": false
+  identifier: "Koi Node V4",
+  password: "prplmoe.me",
+  host: "lavav4.prplmoe.me",
+  port: 1118,
+  secure: false
     },
     {
-      name: "ChalresNaig Node",
-      password: "NAIGLAVA-dash.techbyte.host",
-      host: "lavahatry4.techbyte.host",
-      port: 3000,
+      name: "Koi Node V4",
+      password: "prplmoe.me",
+      host: "lavav4.prplmoe.me",
+      port: 1118,
       secure: false
     }
   ]
