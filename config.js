07617a372160d9efb3f50428e87643b2652a25ab
Opilog <128095264+opilog12@users.noreply.github.com>
2025-02-24 09:06:53 +0100

    Update config.js

--- config.js
+++ config.js
@@ -15,17 +15,17 @@ module.exports = {
   errorLog: "", 
   nodes: [
      {
-  "identifier": "Public Lavalink v4 NonSSL",
-  "password": "https://dsc.gg/ajidevserver",
-  "host": "lava-v4.ajieblogs.eu.org",
-  "port": 80,
+  "identifier": "INZEWORLD.COM (DE)",
+  "password": "saher.inzeworld.com",
+  "host": "lava.inzeworld.com",
+  "port": 3128,
   "secure": false
-                  },
+          },
     {
-  "name": "Public Lavalink v4 NonSSL",
-  "password": "https://dsc.gg/ajidevserver",
-  "host": "lava-v4.ajieblogs.eu.org",
-  "port": 80,
+  "name": "INZEWORLD.COM (DE)",
+  "password": "saher.inzeworld.com",
+  "host": "lava.inzeworld.com",
+  "port": 3128,
   "secure": false
     }
   ]
