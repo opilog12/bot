7fb5ee4f31f3f46f2ea7df42a3f8d471e32d73a7
Opilog <128095264+opilog12@users.noreply.github.com>
2025-03-18 07:22:02 +0100

    Update config.js

--- config.js
+++ config.js
@@ -15,19 +15,19 @@ module.exports = {
   errorLog: "", 
   nodes: [
     {
-   "identifier": "ChalresNaig Node",
-   "password": "NAIGLAVA-dash.techbyte.host",
-   "host": "lavahatry4.techbyte.host",
-   "port": 3000,
-   "secure": false
+  "identifier": "ChalresNaig Node",
+  "password": "NAIGLAVA-dash.techbyte.host",
+  "host": "lavahatry4.techbyte.host",
+  "port": 3000,
+  "secure": false
                          },
     
     {
-   "name": "ChalresNaig Node",
-   "password": "NAIGLAVA-dash.techbyte.host",
-   "host": "lavahatry4.techbyte.host",
-   "port": 3000,
-   "secure": false
+  "name": "ChalresNaig Node",
+  "password": "NAIGLAVA-dash.techbyte.host",
+  "host": "lavahatry4.techbyte.host",
+  "port": 3000,
+  "secure": false
                          },
   ]
 }
